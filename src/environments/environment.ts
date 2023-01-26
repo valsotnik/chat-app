ce3f1a73e30194fb687af804595d2740d8ad35d8
valery.sotnik <cmvaltime@gmail.com>
2023-01-26 10:10:05 +0100

    feat: configurate connection with firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'getstreamio-cmval',
+    appId: '1:162375529037:web:7b4fb94d55189120702204',
+    storageBucket: 'getstreamio-cmval.appspot.com',
+    apiKey: 'AIzaSyAufmvr2AN4BEoAVtQvu9R_fcLkoP57Ca0',
+    authDomain: 'getstreamio-cmval.firebaseapp.com',
+    messagingSenderId: '162375529037',
+    measurementId: 'G-BWHSXL9Y0V',
+  },
   production: false
 };
 
