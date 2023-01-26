26d1e8faa9dd4023c2ce05e4bf12b407d7196146
valery.sotnik <cmvaltime@gmail.com>
2023-01-26 11:12:30 +0100

    feat: add guards for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './features/auth/signin/signin.component';
 import { SignupComponent } from './features/auth/signup/signup.component';
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
 const routes: Routes = [
-  { path: 'signin', component: SigninComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'chat', loadChildren: () => import('./features/chat/chat.module').then(m => m.ChatModule) },
+  { path: '', redirectTo: 'chat', pathMatch: 'full' },
+  {
+    path: 'signin',
+    component: SigninComponent,
+    // protection from entering a '/signin' route manually when already signin state in chat
+    ...canActivate(() => redirectLoggedInTo(['chat']))
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    // protection from entering a '/signup' route manually when already signin state in chat
+    ...canActivate(() => redirectLoggedInTo(['chat']))
+  },
+  {
+    path: 'chat',
+    // protection from unautorized users in '/chat' route
+    ...canActivate(() => redirectUnauthorizedTo(['signin'])),
+    loadChildren: () => import('./features/chat/chat.module').then(m => m.ChatModule)
+  },
 ];
 
 @NgModule({
